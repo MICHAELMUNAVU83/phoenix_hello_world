eb55a7c1cddfae0b579d4c29b6fc6d58e3ffec29
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-10 14:46:47 +0300

    Added user

---
+++ lib/phoenix_hello_world/user.ex
@@ -0,0 +1,17 @@
+defmodule PhoenixHelloWorld.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email])
+    |> validate_required([:name, :email])
+  end
+end
